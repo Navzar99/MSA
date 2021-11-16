6aa47ab4d06aa39c025c4d0f8a335837d88016ec
Navza99 <razvan.oprescu102@gmail.com>
2021-11-16 22:34:16 +0200

    First Commit - added the basic entities and DB setup

---
+++ src/main/java/com/msa/app/repositories/TableRepository.java
@@ -0,0 +1,8 @@
+package com.msa.app.repositories;
+import com.msa.app.entities.Table;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface TableRepository extends CrudRepository<Table, Long> {
+
+}
